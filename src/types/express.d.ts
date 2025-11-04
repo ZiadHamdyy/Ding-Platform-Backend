7c8e1163e3d03eed6adbb884826bbbb2ca72362b
ZiadHamdyy <ziadhamdy667@gmail.com>
2025-11-04 22:09:09 +0200

    Enhance TypeScript configuration by adding typeRoots and extend Express Response interface with cookie methods

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,17 +1,20 @@
 import 'express';
+import 'express-serve-static-core';
 
 declare module 'express-serve-static-core' {
+  interface Response {
+    cookie(name: string, value: string, options?: any): this;
+    clearCookie(name: string, options?: any): this;
+  }
+}
+
+declare module 'express' {
   interface Request {
     headers: {
       [key: string]: string | string[] | undefined;
       authorization?: string;
       Authorization?: string;
     };
-  }
-
-  interface Response {
-    cookie(name: string, value: string, options?: any): this;
-    clearCookie(name: string, options?: any): this;
   }
 }
 
